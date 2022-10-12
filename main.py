612e4c1ab420ab7537bb8e94eba5533457a92010
Amandine Miceli <amandine.miceli@job2day.fr>
2022-10-12 22:41:06 +0200

    Renamed class for auth + added new class for data processing

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ def base64_encode_token(token):
     return base64.b64encode(token.encode()).decode()
 
 
-class Spotify:
-    CLIENT_ID = os.getenv('CLIENT_ID')
+class Auth:
+    CLIENT_ID     = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    REDIRECT_URL = 'http://my.spotify-project.io/callback'
+    REDIRECT_URL  = 'http://my.spotify-project.io/callback'
 
-    AUTH_URL = 'https://accounts.spotify.com/authorize?'
-    AUTH_SCOPE = 'user-read-private user-read-email'
+    AUTH_URL   = 'https://accounts.spotify.com/authorize?'
+    AUTH_SCOPE = 'user-follow-read playlist-read-private user-library-read'
 
-    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    TOKEN_URL    = 'https://accounts.spotify.com/api/token'
 
     def get_auth_code(self):
         state = get_random_code()
@@ -38,7 +38,7 @@ class Spotify:
 
         webbrowser.open(self.AUTH_URL + urlencode(auth_headers))
 
-    def get_auth_token(self, auth_code):
+    def get_access_token(self, auth_code):
         token = self.CLIENT_ID + ':' + self.CLIENT_SECRET
         base64_token = base64_encode_token(token)
 
@@ -56,16 +56,57 @@ class Spotify:
         response = requests.post(self.TOKEN_URL, data=payload, headers=headers)
         return response.json()
 
-    def run(self):
+
+class Spotify(Auth):
+    BASE_URL = 'https://api.spotify.com/v1/'
+    HEADERS  = {'Content-Type': 'application/json'}
+
+    def get_followed_artists(self, after=None):
+        endpoint = self.BASE_URL + 'me/following'
+        payload = {'type': 'artist', 'limit': 50}
+
+        if after is not None:
+            payload['after'] = after
+
+        response = requests.request("GET", endpoint, params=payload, headers=self.HEADERS)
+        return response.json()
+
+    def get_artist(self, artist_id):
+        endpoint = self.BASE_URL + 'artists/' + str(artist_id)
+        response = requests.request("GET", endpoint, params={}, headers=self.HEADERS)
+        return response.json()
+
+    def get_auth(self):
         # get authorization code
-        self.get_auth_code()
+        Auth.get_auth_code(self)
 
         auth_code = input("Enter code from redirect URL here: ")
 
         # exchange authorization code against access token
-        response = self.get_auth_token(auth_code)
-        access_token = response.get('access_token')
-        print(access_token)
+        response = Auth.get_access_token(self, auth_code)
+
+        # refresh_token = response.get('refresh_token')
+        return response.get('access_token')
+
+    def run(self):
+        access_token = self.get_auth()
+        self.HEADERS['Authorization'] = 'Bearer ' + access_token
+
+        # retrieve followed artists
+        artists = []
+        next_page = True
+        after = None
+
+        while next_page:
+            response = self.get_followed_artists(after)
+            after = response.get('artists').get('cursors').get('after')
+            artists += response.get('artists').get('items')
+            if after is None:
+                next_page = False
+
+        for artist in artists:
+            artist = self.get_artist(artist.get('id'))
+            print(artist.get('name'))
 
 
 Spotify().run()
